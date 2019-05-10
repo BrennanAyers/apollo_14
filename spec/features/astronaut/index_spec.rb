bf6771bf555b43e8a48c7979d35169feaacbc1fe
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-10 09:19:12 -0600

    Add initial view spec for Astronauts/index

--- spec/features/astronaut/index_spec.rb
+++ spec/features/astronaut/index_spec.rb
@@ -8,7 +8,25 @@ RSpec.describe 'As a user', type: :feature do
       @alex = Astronaut.create!(name: "Alex Armstrong", age: 25, job: "Chef")
     end
     it 'I see a list of astronauts with their info' do
+      visit '/astronauts'
 
+      within("#astronaut-#{@neil.id}") do
+        expect(page).to have_content("Name: #{@neil.name}")
+        expect(page).to have_content("Age: #{@neil.age}")
+        expect(page).to have_content("Job: #{@neil.job}")
+      end
+
+      within("#astronaut-#{@logan.id}") do
+        expect(page).to have_content("Name: #{@logan.name}")
+        expect(page).to have_content("Age: #{@logan.age}")
+        expect(page).to have_content("Job: #{@logan.job}")
+      end
+
+      within("#astronaut-#{@alex.id}") do
+        expect(page).to have_content("Name: #{@alex.name}")
+        expect(page).to have_content("Age: #{@alex.age}")
+        expect(page).to have_content("Job: #{@alex.job}")
+      end
     end
   end
 end
