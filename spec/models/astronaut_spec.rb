b8e2474b4fda7506b1ef106b33d672328be906e9
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-10 09:37:20 -0600

    Add initial Astronaut::average_age spec to Astronaut model

--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -11,4 +11,16 @@ describe Astronaut, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many :missions}
   end
+
+  describe 'Class methods' do
+    before :each do
+      @neil = Astronaut.create!(name: "Neil Armstrong", age: 37, job: "Commander")
+      @logan = Astronaut.create!(name: "Logan Armstrong", age: 14, job: "Janitor")
+      @alex = Astronaut.create!(name: "Alex Armstrong", age: 25, job: "Chef")
+    end
+
+    it 'should return the average age of collection of Astronauts' do
+      expect(Astronaut.average_age).to eq(25.333333)
+    end
+  end
 end
