c413d4a35d9172cd020132f62b974a813299beb4
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-10 09:53:40 -0600

    Add initial missions spec to Astronauts/index, with more before :each

--- spec/features/astronaut/index_spec.rb
+++ spec/features/astronaut/index_spec.rb
@@ -6,6 +6,16 @@ RSpec.describe 'As a user', type: :feature do
       @neil = Astronaut.create!(name: "Neil Armstrong", age: 37, job: "Commander")
       @logan = Astronaut.create!(name: "Logan Armstrong", age: 14, job: "Janitor")
       @alex = Astronaut.create!(name: "Alex Armstrong", age: 25, job: "Chef")
+
+      @capricorn_4 = @neil.missions.create!(title: "Capricorn 4", time_in_space: 280)
+      @apollo_13 = @neil.missions.create!(title: "Apollo 13", time_in_space: 43)
+      @gemini_7 = @neil.missions.create!(title: "Gemini 7", time_in_space: 55)
+
+      @logan.missions << @gemini_7
+      @logan.missions << @capricorn_4
+
+      @lost = @alex.missions.create!(title: "Lost on Mars", time_in_space: 5067)
+      @alex.missions << @apollo_13
     end
     it 'I see a list of astronauts with their info' do
       visit astronauts_path
@@ -35,6 +45,26 @@ RSpec.describe 'As a user', type: :feature do
       expected = (@neil.age + @alex.age + @logan.age) / 3.0
 
       expect(page).to have_content("Average Age: #{expected.round(2)}")
+    end
+
+    it 'I see the missions for each Astronaut alphabetically' do
+      visit astronauts_path
+
+      within("#astronaut-#{@neil.id}-missions") do
+        expect(page).to have_content("#{@apollo_13}")
+        expect(page).to have_content("#{@capricorn_4}")
+        expect(page).to have_content("#{@gemini_7}")
+      end
+
+      within("#astronaut-#{@logan.id}-missions") do
+        expect(page).to have_content("#{@capricorn_4}")
+        expect(page).to have_content("#{@gemini_7}")
+      end
+
+      within("#astronaut-#{@alex.id}-missions") do
+        expect(page).to have_content("#{@apollo_13}")
+        expect(page).to have_content("#{@lost}")
+      end
     end
   end
 end
